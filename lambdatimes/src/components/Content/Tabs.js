a4a28aeae329113b115bc27a0b959bca42ed6f69
Jason Cook <the.root.user@gmail.com>
2019-03-24 22:05:49 -0400

    Setup tab prop by mapping over the array of tabs.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,11 +5,24 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(tab => {
+          return (
+            <Tab
+              tab={tab}
+              tabHandler={props.tabHandler}
+              tabSelected={props.tabSelected}
+            />
+          );
+        })}
       </div>
     </div>
   );
+};
+
+Tabs.propTypes = {
+  tab: PropTypes.array,
+  tabSelected: PropTypes.func,
+  tabHandler: PropTypes.func
 };
 
 // Make sure to use PropTypes to validate your types!
