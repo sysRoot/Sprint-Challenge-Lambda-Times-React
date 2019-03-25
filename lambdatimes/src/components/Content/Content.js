a2e7def7e4ebae9db07027591ffc51d1b0c119d0
Jason Cook <the.root.user@gmail.com>
2019-03-24 22:09:55 -0400

    Import PropTypes, almost forgot
    Fixed missing string marker in content.
    Yarn had stopped the node server
    I didn't notice errors because of this.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -46,7 +46,7 @@ export default class Content extends Component {
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
     return this.state.cards;
-    this.state.selected == all ? this.state.cards : this.state.cards.filter(cur => cur.tab == this.state.selected);
+    this.state.selected == 'all' ? this.state.cards : this.state.cards.filter(cur => cur.tab == this.state.selected);
   };
 
   render() {
