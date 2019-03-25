df6252a351b6e142e9c4780807b2bc05721f9e40
Jason Cook <the.root.user@gmail.com>
2019-03-24 23:24:55 -0400

    Begin working on stretch

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import styled from 'styled-components'
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default class Content extends Component {
   constructor(props) {
@@ -54,7 +61,7 @@ export default class Content extends Component {
 
   render() {
     return (
-      <div className="content-container">
+      <ContentContainer>
         {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
@@ -66,7 +73,7 @@ export default class Content extends Component {
         tabHandler={this.changeSelected}
         />
         <Cards cards={this.filterCards()} />
-      </div>
+        </ContentContainer>
     );
   }
 }
